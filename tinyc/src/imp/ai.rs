5ceb3e6360cf08b73f8ce782bb281950230868bd
Russel Arbore <russel.jma@gmail.com>
2026-05-03 11:45:45 -0700

    Scrap old SSABuilder, it was trash

--- tinyc/src/imp/ai.rs
+++ tinyc/src/imp/ai.rs
@@ -2,15 +2,11 @@ use core::cell::RefCell;
 
 use rustc_hash::FxHashMap;
 
-use crate::imp::ast::FuncAST;
-use crate::ssa::{SSABuilder, SSAProgram};
+use crate::imp::ast::{ExprAST, FuncAST, StmtAST};
+use crate::ssa::SSAProgram;
 
 pub fn create_ssa<'a>(ast: &'a FxHashMap<String, FuncAST>) -> SSAProgram<'a> {
     let ssa = RefCell::new(Default::default());
-
-    if let Some(main) = ast.get("main") {
-        let ctx = SSABuilder::entry(&main.name, main.params.iter().map(|s| s as _), &ssa);
-    }
 
     ssa.into_inner()
 }
--- tinyc/src/ssa.rs
+++ tinyc/src/ssa.rs
@@ -1,6 +1,3 @@
-use core::cell::RefCell;
-use core::mem::replace;
-
 use derive_more::FromStr;
 use egg::{EGraph, Id, define_language};
 use rustc_hash::FxHashMap;
@@ -25,20 +22,20 @@ pub enum BinaryOp {
 }
 
 pub type BlockId = usize;
-type ParamId = usize;
-type KnotId = usize;
-type CallId = usize;
+pub type ParamId = usize;
+pub type KnotId = usize;
+pub type CallId = usize;
 
 define_language! {
     pub enum Dataflow {
         Constant(i64),
-        Param(ParamId),
-        Phi(BlockId, [Id; 2]),
         Unary(UnaryOp, Id),
         Binary(BinaryOp, [Id; 2]),
+        Phi(BlockId, [Id; 2]),
         Load(BlockId, Id),
-        Call(CallId),
+        Param(ParamId),
         Knot(KnotId),
+        Call(CallId),
     }
 }
 
@@ -49,7 +46,7 @@ pub enum Block<'a> {
     Merge(BlockId, BlockId),
     Store(BlockId, Id, Id),
     Call(BlockId, &'a str, Vec<Id>),
-    Return(BlockId, Id),
+    Return(BlockId, Vec<Id>),
 }
 
 #[derive(Debug, Default)]
@@ -61,8 +58,8 @@ pub struct SSAProgram<'a> {
     param_map: FxHashMap<(&'a str, usize), ParamId>,
     // Intern pairs of BlockId and variable name to KnotId.
     knot_map: FxHashMap<(BlockId, &'a str), KnotId>,
-    // Intern pairs of BlockId (of Call blocks) and return value index to CallId.
-    call_map: FxHashMap<(BlockId, usize), CallId>,
+    // Intern pairs of function name and return value index to CallId.
+    call_map: FxHashMap<(&'a str, usize), CallId>,
 }
 
 impl<'a> SSAProgram<'a> {
@@ -76,12 +73,12 @@ impl<'a> SSAProgram<'a> {
         id
     }
 
-    fn intern_param(&mut self, var: &'a str, idx: usize) -> ParamId {
-        if let Some(id) = self.param_map.get(&(var, idx)) {
+    fn intern_param(&mut self, function: &'a str, idx: usize) -> ParamId {
+        if let Some(id) = self.param_map.get(&(function, idx)) {
             *id
         } else {
             let id = self.param_map.len();
-            self.param_map.insert((var, idx), id);
+            self.param_map.insert((function, idx), id);
             id
         }
     }
@@ -96,170 +93,13 @@ impl<'a> SSAProgram<'a> {
         }
     }
 
-    fn intern_call(&mut self, block: BlockId, idx: usize) -> ParamId {
-        if let Some(id) = self.call_map.get(&(block, idx)) {
+    fn intern_call(&mut self, function: &'a str, idx: usize) -> ParamId {
+        if let Some(id) = self.call_map.get(&(function, idx)) {
             *id
         } else {
             let id = self.call_map.len();
-            self.call_map.insert((block, idx), id);
+            self.call_map.insert((function, idx), id);
             id
         }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct SSABuilder<'a, 'b> {
-    ssa: &'b RefCell<SSAProgram<'a>>,
-
-    vars: FxHashMap<&'a str, Id>,
-    function: &'a str,
-    block: BlockId,
-}
-
-impl<'a, 'b> SSABuilder<'a, 'b> {
-    pub fn entry<I>(function: &'a str, params: I, ssa: &'b RefCell<SSAProgram<'a>>) -> Self
-    where
-        I: Iterator<Item = &'a str>,
-    {
-        let block = ssa.borrow_mut().add_block(Block::Entry);
-        SSABuilder {
-            ssa,
-            vars: params
-                .enumerate()
-                .map(|(idx, var)| {
-                    let mut ssa = ssa.borrow_mut();
-                    let param_id = ssa.intern_param(var, idx);
-                    let value = ssa.dfg.add(Dataflow::Param(param_id));
-                    (var, value)
-                })
-                .collect(),
-            function,
-            block,
-        }
-    }
-
-    pub fn assign(&mut self, var: &'a str, value: Id) {
-        self.vars.insert(var, value);
-    }
-
-    pub fn number(&self, num: i64) -> Id {
-        self.ssa.borrow_mut().add_data(Dataflow::Constant(num))
-    }
-
-    pub fn variable(&self, var: &str) -> Id {
-        self.vars[var]
-    }
-
-    pub fn unary(&self, op: UnaryOp, input: Id) -> Id {
-        self.ssa.borrow_mut().add_data(Dataflow::Unary(op, input))
-    }
-
-    pub fn binary(&self, op: BinaryOp, lhs: Id, rhs: Id) -> Id {
-        self.ssa
-            .borrow_mut()
-            .add_data(Dataflow::Binary(op, [lhs, rhs]))
-    }
-
-    pub fn load(&self, pointer: Id) -> Id {
-        self.ssa
-            .borrow_mut()
-            .add_data(Dataflow::Load(self.block, pointer))
-    }
-
-    pub fn store(&mut self, pointer: Id, value: Id) {
-        self.block = self
-            .ssa
-            .borrow_mut()
-            .add_block(Block::Store(self.block, pointer, value));
-    }
-
-    pub fn is_always_false(&self, value: Id) -> bool {
-        self.ssa.borrow().dfg[value]
-            .iter()
-            .any(|data| *data == Dataflow::Constant(0))
-    }
-
-    pub fn branch(&self, cond: Id) -> Option<Self> {
-        if self.is_always_false(cond) {
-            None
-        } else {
-            let block = self
-                .ssa
-                .borrow_mut()
-                .add_block(Block::Child(self.block, cond));
-            let mut branched = self.clone();
-            branched.block = block;
-            Some(branched)
-        }
-    }
-
-    fn intersect<'c>(
-        a: &'c FxHashMap<&'a str, Id>,
-        b: &'c FxHashMap<&'a str, Id>,
-        dfg: &'c EGraph<Dataflow, ()>,
-    ) -> impl Iterator<Item = (&'a str, Id)> + 'c {
-        a.into_iter().filter_map(|(name, a)| {
-            if let Some(b) = b.get(name)
-                && dfg.find(*a) == dfg.find(*b)
-            {
-                Some((*name, dfg.find(*a)))
-            } else {
-                None
-            }
-        })
-    }
-
-    fn difference<'c>(
-        a: &'c FxHashMap<&'a str, Id>,
-        b: &'c FxHashMap<&'a str, Id>,
-        dfg: &'c EGraph<Dataflow, ()>,
-    ) -> impl Iterator<Item = (&'a str, Id, Id)> + 'c {
-        a.into_iter().filter_map(|(name, a)| {
-            if let Some(b) = b.get(name)
-                && dfg.find(*a) != dfg.find(*b)
-            {
-                Some((*name, dfg.find(*a), dfg.find(*b)))
-            } else {
-                None
-            }
-        })
-    }
-
-    pub fn forward_merge(a: &Self, b: &Self) -> Self {
-        assert_eq!(a.function, b.function);
-        let block = a.ssa.borrow_mut().add_block(Block::Merge(a.block, b.block));
-        let mut vars: FxHashMap<&str, Id> =
-            Self::intersect(&a.vars, &b.vars, &a.ssa.borrow().dfg).collect();
-        let differences: Vec<_> = Self::difference(&a.vars, &b.vars, &a.ssa.borrow().dfg).collect();
-        for (name, a_value, b_value) in differences {
-            let phi = a
-                .ssa
-                .borrow_mut()
-                .add_data(Dataflow::Phi(block, [a_value, b_value]));
-            vars.insert(name, phi);
-        }
-        SSABuilder {
-            ssa: a.ssa,
-            vars,
-            function: a.function,
-            block,
-        }
-    }
-
-    pub fn backward_merge(&mut self, other: &Self) {
-        assert_eq!(self.function, other.function);
-        let old_self_block = replace(&mut self.ssa.borrow_mut().cfg[self.block], Block::Entry);
-        let old_self_block = self.ssa.borrow_mut().add_block(old_self_block);
-        self.ssa.borrow_mut().cfg[self.block] = Block::Merge(old_self_block, other.block);
-        let mut vars: FxHashMap<&str, Id> =
-            Self::intersect(&self.vars, &other.vars, &self.ssa.borrow().dfg).collect();
-        let differences: Vec<_> =
-            Self::difference(&self.vars, &other.vars, &self.ssa.borrow().dfg).collect();
-        for (name, _, _) in differences {
-            let knot = self.ssa.borrow_mut().intern_knot(self.block, name);
-            let knot = self.ssa.borrow_mut().add_data(Dataflow::Knot(knot));
-            vars.insert(name, knot);
-        }
-        self.vars = vars;
     }
 }
